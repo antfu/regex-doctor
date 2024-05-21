b926ea197a595da5195f94d2314647e2c48b1519
libondev <bon.li@outlook.com>
2024-05-21 12:00:03 +0800

    style: files icon is squeezed

--- ui/components/RegexTable.vue
+++ ui/components/RegexTable.vue
@@ -135,7 +135,7 @@ function onFilterConfirm(conditions: ConditionGetter[]) {
                 v-if="data.filesCreated?.length" i-ph-file-code-duotone text-purple
                 title="Regex creation source"
               />
-              <div op50 i-ph-files-duotone />
+              <div op50 i-ph-files-duotone flex-shrink-0 />
               {{ data.filesCalled?.length }}
             </button>
             <template #popper>
